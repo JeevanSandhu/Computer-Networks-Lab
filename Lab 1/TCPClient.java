90df26d1b7ba08dbc3f9ef42bdafab9668b6e29c
JeevanSandhu <JeevanSandhu@users.noreply.github.com>
2017-02-09 11:43:48 +0400

    Add files via upload

---
+++ Lab 1/TCPClient.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+
+public class TCPClient
+{
+	public static void main(String[] args) throws Exception
+	{
+		//create socket object at the client to connect to server
+		
+		Socket client= new Socket("127.0.0.1", 6969);
+		//now 3 way handshaking 
+
+		//client will send and receive data to/from server
+		System.out.println("Connected to server");
+
+		BufferedReader inputServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		PrintWriter outputServer = new PrintWriter(client.getOutputStream());
+		OutToServer.println("Hi from Client!");
+		OutToServer.flush();
+		String received = InputServer.readLine();
+		System.out.println("Received from server: " + received);
+	}    
+}
