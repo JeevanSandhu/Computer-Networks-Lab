883f9ddc432a2bb5d133bcba583d8fe2b37a2b8f
JeevanSandhu <JeevanSandhu@users.noreply.github.com>
2017-02-17 13:21:47 +0400

    Update TCPServer.java

--- Lab 1/TCPServer.java
+++ Lab 1/TCPServer.java
@@ -13,46 +13,48 @@ public class TCPServer implements Runnable
 
     public void run()
     {
-        try
+        //This while loop is to serve each client multiple times
+        while(true)
         {
-            //Attach an input stream and outputstream to send/receive via the perclientsocket
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            try
+            {
+                //Attach an input stream and outputstream to send/receive via the perclientsocket
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
            
-            //Accumulate all the input in a line. That is why we create a buffer...
-            PrintWriter output = new PrintWriter(socket.getOutputStream());
+                //Create a buffer to accumulate all the input in a line
+                PrintWriter output = new PrintWriter(socket.getOutputStream());
       
-            //Attach an input stream and outputstream
-            //to send/receive via the clientsocket
-            String fromClient = input.readLine();
-            System.out.println("Received  : " + fromClient);
+                String fromClient = input.readLine();
+                System.out.println("Received  : " + fromClient);
 
-            String toClient = fromClient.toUpperCase();
-            System.out.println("Sent  : " + toClient);
+                String toClient = fromClient.toUpperCase();
+                System.out.println("Sent  : " + toClient);
             
-            output.println(toClient);
-            output.flush();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
+                output.println(toClient);
+                output.flush();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
         }
     }
     
     public static void main(String[] args) throws Exception
     {
-        //create a ServerSocket via port number 6789 and wait for connection from client
+        //create a ServerSocket via port number 6969 and wait for connection from client
         ServerSocket serverSocketObj = new ServerSocket(6969);
-        //the server application will be assigned address of 6969
 
+        //This while loop is to serve multiple clients
         while(true)
         {
             System.out.println("Server is listening on port 6969 ...");
+            
             //Listen for connection from client
             Socket clientSocket = serverSocketObj.accept();
 
             //start a client thread to serve the connected client using clientsocket
             TCPServer TCPServerObj = new TCPServer(clientSocket);
-            
             //create a thread for each client...
             Thread newClient = new Thread(TCPServerObj);
             newClient.start();
